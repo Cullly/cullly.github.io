dfb05417181983264ac0000dc8fbfc65af1ebeef
Jesse Kurtz <jessedkurtz@gmail.com>
2022-03-15 15:59:06 -0400

    Update artstop.js

--- artstop.js
+++ artstop.js
@@ -4,8 +4,5 @@ AFRAME.registerComponent('artstop', {
     },
     init: function() {
         console.log('Initializing: ' + this.data.name);
-        console.log(this.el)
-        console.log(this.data.distanceMsg);
-        console.log(this.el.getAttribute('distanceMsg'));
     }
 });
